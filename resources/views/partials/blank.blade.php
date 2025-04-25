e79311707c727cc613e330b2d313a3960fe06349
Reza <muhammad.safreza1@gmail.com>
2025-04-26 06:47:04 +0700

    feat: :tada: First Commit

---
+++ resources/views/partials/blank.blade.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html lang="en">
+<body class="bg-gray-200">
+@include('partials.head-sign')
+@yield('content')
+@include('partials.js-sign')
+</body>
+</html>
---
+++ resources/views/partials/head-sign.blade.php
@@ -0,0 +1,21 @@
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('assets/img/apple-icon.png') }}">
+  <link rel="icon" type="image/png" href="{{ asset('assets/img/favicon.png') }}">
+  <title>
+    {{ config('app.APP_NAME') }}
+  </title>
+  <!--     Fonts and icons     -->
+  <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Inter:300,400,500,600,700,900" />
+  <!-- Nucleo Icons -->
+  <link href="{{ asset('assets/css/nucleo-icons.css') }}" rel="stylesheet" />
+  <link href="{{ asset('assets/css/nucleo-svg.css') }}" rel="stylesheet" />
+  <!-- Font Awesome Icons -->
+  <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+  <!-- Material Icons -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@24,400,0,0" />
+  <!-- CSS Files -->
+  <link id="pagestyle" href="{{ asset('assets/css/material-dashboard.css?v=3.2.0') }}" rel="stylesheet" />
+</head>
